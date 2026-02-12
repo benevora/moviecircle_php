1464d18f906e80b81e649abfa5731ab9365ebbc6
Ben Evora <65423996+benevora@users.noreply.github.com>
2026-02-12 17:35:54 -0500

    project structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MovieCircle</title>
+  <link rel="short icon" href="img/moviecircle.ico">
+  <!-- project css -->
+</head>
+<body>
+  <h1>Testing Moviecircle</h1>
+</body>
+</html>
