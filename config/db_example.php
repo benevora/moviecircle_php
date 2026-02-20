c4d3e22970279267ec727602b669033697fea6d0
Ben Evora <benevora05@gmail.com>
2026-02-19 21:02:41 -0500

    fixing bug in the project structure v02

---
+++ config/db_example.php
@@ -0,0 +1,16 @@
+<?php
+
+  $db_name = "moviecircle";
+  $db_host = "localhost";
+  $db_user = "root";
+  $db_pass = "";
+
+  // connection
+  $conn = new PDO(
+    "mysql:dbname=$db_name;host=$db_host;charset=utf8mb4",
+    $db_user,
+    $db_pass);
+
+  // enable PDO errors
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
