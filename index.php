ed3f5b6b2327f575656dd0f8733a3ed1c00838ba
Ben Evora <benevora05@gmail.com>
2026-02-26 19:26:50 -0500

    home page create

--- index.php
+++ index.php
@@ -1,33 +1,28 @@
 <?php
+  /* ======================================
+     HEADER TEMPLATE
+     - Includes global configuration
+     - Loads HTML <head>
+     - Displays navigation bar
+  ====================================== */
 
-  require_once("globals.php");
-  require_once("config/db.php");
-
+  // Load header template (HTML head + navbar)
+  require_once("templates/header.php");
 ?>
 
+  <!-- ========== MAIN CONTENT ========= -->
+  <div id="main-container" class="container-fluid">
+    <h1>Website body</h1>
+  </div>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MovieCircle</title>
-  <link rel="short icon" href="img/moviecircle.ico">
-  <!-- Bootstrap css-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap.css" integrity="sha512-zylce7fP6h4usg536JBTRj2rt7q22Z0qicHSlgSK53Irtfkz37ate3KCQ59du+aXZV6R3yyL2X1LyGKBEUMZaw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<?php
+  /* ======================================
+     FOOTER TEMPLATE
+     - Closes main layout structure
+     - Loads footer section
+     - Includes JavaScript files
+  ====================================== */
 
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-  <!-- project css -->
-  <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-  <h1>Testing Moviecircle</h1>
-  <i class="far fa-plus-square"></i>
-  <!-- Bootstrap js -->
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/js/bootstrap.js" integrity="sha512-GKxQGmCBqm3YHCZTdqD3Bvsz+AHRObqztq6xDo+68527RxgVw7wF1TR5ZSBvtWiz5qzpOzUGGvxJbT/6UzzSOQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+  // Load footer template (footer + scrips)
+  require_once("templates/footer.php");
+?>
