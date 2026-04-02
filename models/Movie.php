5a5ddb4025683912a482e7c4e64cebad1a17bf61
AlexQuesada36 <alexq1710@gmail.com>
2026-04-02 16:25:35 -0400

    Review Page

--- models/Movie.php
+++ models/Movie.php
@@ -15,18 +15,46 @@
       return bin2hex(random_bytes(60)) . "." . $extension;
     }
 
+    public function getTrailerEmbed() {
+
+      // Normal YouTube link
+      if(strpos($this->trailer, "youtube.com") !== false) {
+        parse_str(parse_url($this->trailer, PHP_URL_QUERY), $params);
+        if(isset($params['v'])) {
+          return "https://www.youtube.com/embed/" . $params['v'];
+        }
+      }
+
+      // Short youtu.be link
+      if(strpos($this->trailer, "youtu.be") !== false) {
+        $videoId = basename(parse_url($this->trailer, PHP_URL_PATH));
+        return "https://www.youtube.com/embed/" . $videoId;
+      }
+
+      return $this->trailer;
+    }
+
   }
 
 
   interface MovieDAOInterface {
     public function buildMovie($data);
+
     public function findAll();
+
     public function getLatestMovies();
+
     public function getMoviesByCategory($category);
-    public function getMoviesByUserID($id);
+
+    public function getMoviesByUserId($id);
+
     public function findById($id);
+
     public function findByTitle($title);
+
     public function create(Movie $movie);
+
     public function update(Movie $movie);
+
     public function destroy($id);
   }
